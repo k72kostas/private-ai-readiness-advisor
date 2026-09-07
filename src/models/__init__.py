6773b4a206706cc755e767b4cc9e241c3b581e26
Konstantinos Kaminaris <kkaminaris@gmail.com>
2026-09-07 17:32:33 +0300

    feat: add assessment domain models

---
+++ src/models/__init__.py
@@ -0,0 +1,25 @@
+from .assessment import (
+    Assessment,
+    AssessmentResponse,
+    AssessmentStatus,
+    DataSensitivity,
+    DeploymentBoundary,
+    EvidenceReference,
+    EvidenceStatus,
+    EvidenceType,
+    OrganizationProfile,
+    TargetStage,
+)
+
+__all__ = [
+    "Assessment",
+    "AssessmentResponse",
+    "AssessmentStatus",
+    "DataSensitivity",
+    "DeploymentBoundary",
+    "EvidenceReference",
+    "EvidenceStatus",
+    "EvidenceType",
+    "OrganizationProfile",
+    "TargetStage",
+]
